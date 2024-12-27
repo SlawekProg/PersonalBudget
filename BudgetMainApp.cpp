3a456f9a2e751a0055c12150fcb531536f2f213c
Slawomir Banas <slawomir.banas.programista@gmail.com>
2024-12-27 13:16:38 +0100

    Add Menu in Main

--- BudgetMainApp.cpp
+++ BudgetMainApp.cpp
@@ -1,3 +1,35 @@
 #include "BudgetMainApp.h"
 
 using namespace std;
+
+    bool BudgetMainApp::isUserLoggedIn(){
+
+    }
+    void BudgetMainApp::registerUser(){
+
+    }
+    void BudgetMainApp::loginUser(){
+
+    }
+    void BudgetMainApp::changeUserPassword(){
+
+    }
+    void BudgetMainApp::logoutUser(){
+
+    }
+
+    void BudgetMainApp::addIncome(){
+
+    }
+    void BudgetMainApp::addExpense(){
+
+    }
+    void BudgetMainApp::showCurrentMonthBalance(){
+
+    }
+    void BudgetMainApp::showPreviousMonthBalance(){
+
+    }
+    void BudgetMainApp::showCustomPeriodBalance(){
+
+    }
--- Utils.cpp
+++ Utils.cpp
@@ -1,3 +1,14 @@
 #include "Utils.h"
 
 using namespace std;
+
+char Utils::getCharacter(){
+    cout << "wpisalem znak";
+    return '9';
+};
+string Utils::readLine(){
+    return "0";
+};
+bool Utils::validateInput(string input){
+    return 1;
+}
